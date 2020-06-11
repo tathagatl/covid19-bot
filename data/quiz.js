73f63a3a73d560776c79d45c3f05c9ca7f84cd53
Tathagat Lokhande <tathagatlokhande@gmail.com>
2020-06-11 10:54:44 +0530

    Commiting Quiz Updates

--- data/quiz.js
+++ data/quiz.js
@@ -8,7 +8,7 @@ module.exports = [
         "answer": "d"
     },
     {
-        "question": "What is COVID-19?",
+        "question": "What is COVID-19",
         "option1": "Name of the Virus",
         "option2": "Name of the disease",
         "option3": "Name of a drug",
@@ -16,7 +16,7 @@ module.exports = [
         "answer": "b"
     },
     {
-        "question": "Which virus causes the disease COVID-19",
+        "question": "Which virus causes the disease COVID-19?",
         "option1": "SARS-CoV-2",
         "option2": "SARS",
         "option3": "MERS",
@@ -64,7 +64,7 @@ module.exports = [
         "answer": "d"
     },
     {
-        "question": "What are the safe methods of disposal of dead bodies of persons who have died of suspected or confirmed COVID-19",
+        "question": "What are the safe methods of disposal of dead bodies of persons who have died of suspected or confirmed COVID-19?",
         "option1": "Cremation",
         "option2": "Burial",
         "option3": "Both 1 & 2",
@@ -76,5 +76,77 @@ module.exports = [
         "option1": "Yes",
         "option2": "No",
         "answer": "b"
+    },
+    {
+        "question": "Coronaviruses are named for the spikes that protrude from their surfaces, resembling. They can infect both animals and people, and can cause illnesses of the respiratory tract.",
+        "option1": "Shark’s teeth",
+        "option2": "Barbed wire",
+        "option3": "Football cleats",
+        "option4": "The Sun’s corona",
+        "answer": "d"
+    },
+    {
+        "question": "The coronavirus spreads easily from person to person, traveling through the air, enveloped in tiny  or picked up when a person touches an infected surface.",
+        "option1": "Bacterial follicles",
+        "option2": "Gamma rays",
+        "option3": "Pollution particles",
+        "option4": "Viral droplets",
+        "answer": "d"
+    },
+    {
+        "question": "Which of the following is NOT a common symptom of the coronavirus?",
+        "option1": "Dry Cough",
+        "option2": "Fever",
+        "option3": "Rash",
+        "option4": "Shortness of breath",
+        "answer": "c"
+    },
+    {
+        "question": "Most people fall ill five to seven days after exposure, but symptoms may appear in as few as two days or as many as days.",
+        "option1": "14",
+        "option2": "21",
+        "option3": "28",
+        "option4": "35",
+        "answer": "a"
+    },
+    {
+        "question": "A vaccine for the coronavirus will take at least  to become available to the public.",
+        "option1": "Six weeks",
+        "option2": "Three months",
+        "option3": "Six months",
+        "option4": "One year",
+        "answer": "d"
+    },
+    {
+        "question": "Which of the following disease is caused by the novel coronavirus?",
+        "option1": "SARS-CoV-2",
+        "option2": "COVID-19",
+        "option3": "Hanta Pulmunory Syndrome",
+        "option4": "MERS",
+        "answer": "b"
+    },
+    {
+        "question": "Which of the following precautions are useful in preventing the COVID-19?",
+        "option1": "Washing hands with soap",
+        "option2": "Covering nose and mouth with handkerchief",
+        "option3": "Using hand-sanitizer",
+        "option4": "All three of the above",
+        "answer": "d"
+    },
+    {
+        "question": "What is the period between the contraction of the virus and the manifestation of the disease called?",
+        "option1": "Incubation Period",
+        "option2": "Infection Period",
+        "option3": "Expectation Period",
+        "option4": "Multiplication Period",
+        "answer": "a"
+    },
+    {
+        "question": "At a time when all our lives are disrupted and many face increased loneliness and anxiety, experts recommend that people stay connected in all of the following ways EXCEPT :",
+        "option1": "Participating in online game nights",
+        "option2": "Participating in online learning",
+        "option3": "Connecting via social media, chat and video",
+        "option4": "Visiting others at home for short periods of time",
+        "answer": "d"
     }
 ]
